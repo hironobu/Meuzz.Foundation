26023219408da6ad69bcc263b30b0cd807405e84
hironobu koura <hironobu.k@mac.com>
2021-03-28 00:07:51 +0900

    init

---
+++ Meuzz.Foundation/Arrays.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace Meuzz.Foundation
+{
+    public class Arrays
+    {
+        public static bool IsNullOrEmpty<T>(T[] arr)
+        {
+            return arr == null || !arr.Any();
+        }
+
+        public static bool Equals<T>(T[] arr1, T[] arr2)
+        {
+            if (arr1.Length != arr2.Length)
+                return false;
+
+            return arr1.Zip(arr2, (a, b) => a.Equals(b)).All(x => x);
+        }
+
+        public static T[] Concat<T>(params T[][] arrs)
+        {
+            var result = new T[arrs.Select((x) => x.Length).Aggregate((x, y) => x + y)];
+            int i = 0;
+            foreach (var a in arrs)
+            {
+                Array.Copy(a, 0, result, i, a.Length);
+                i += a.Length;
+            }
+            return result;
+        }
+    }
+}
---
+++ Meuzz.Foundation/DataSet.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Meuzz.Foundation
+{
+    public class DataSet<C, T>
+    {
+        public DataSet()
+        {
+        }
+
+        public DataSet(C[] keys, C[] cols, T[] rows)
+        {
+            KeyColumns = keys;
+            Columns = cols;
+            Rows = rows;
+        }
+
+        public C[] KeyColumns { get; set; }
+
+        public C[] Columns { get; set; }
+
+        public T[] Rows { get; set; }
+
+        public static T1 CreateWithFilteringColumns<T1>(T1 source, C[] cols) where T1 : DataSet<C, T>, new()
+        {
+            return new T1() { KeyColumns = source.KeyColumns, Columns = source.Columns.Where(x => cols.Contains(x)).ToArray(), Rows = source.Rows };
+        }
+
+        public static T1 CreateWithExcludingColumns<T1>(T1 source, C[] cols) where T1 : DataSet<C, T>, new()
+        {
+            return new T1() { KeyColumns = source.KeyColumns, Columns = source.Columns.Where(x => !cols.Contains(x)).ToArray(), Rows = source.Rows };
+        }
+
+        public static T1 CreateWithFilteringRows<T1>(T1 source, Func<T, bool> predicate) where T1 : DataSet<C, T>, new()
+        {
+            return new T1() { KeyColumns = source.KeyColumns, Columns = source.Columns, Rows = source.Rows.Where(x => predicate(x)).ToArray() };
+        }
+
+        public static T1 CreateWithExcludingRows<T1>(T1 source, Func<T, bool> predicate) where T1 : DataSet<C, T>, new()
+        {
+            return new T1() { KeyColumns = source.KeyColumns, Columns = source.Columns, Rows = source.Rows.Where(x => !predicate(x)).ToArray() };
+        }
+
+        /// <summary>
+        /// 2つ以上のDataSetを連結する
+        /// </summary>
+        /// <param name="dataSets"></param>
+        public static T1 CreateFromDataSets<T1>(IEnumerable<T1> dataSets) where T1 : DataSet<C, T>, new()
+        {
+            C[] cols = { };
+            T[] rows = { };
+
+            if (dataSets.Any())
+            {
+                cols = dataSets.FirstOrDefault()?.Columns;
+                rows = Arrays.Concat<T>(dataSets.Select((x) => x.Rows).ToArray());
+            }
+
+            return new T1() { KeyColumns = dataSets.FirstOrDefault()?.KeyColumns, Columns = cols, Rows = rows };
+        }
+
+    }
+
+    public static class DataSetExtensions
+    {
+        [Obsolete]
+        /// <summary>
+        /// 2つ以上のDataSetを連結する
+        /// </summary>
+        /// <param name="dataSets"></param>
+        public static T2 Concat<C, T, T2>(this IEnumerable<T2> dataSets) where T2 : DataSet<C, T>, new()
+        {
+            C[] cols = { };
+            T[] rows = { };
+
+            if (dataSets.Any())
+            {
+                cols = dataSets.FirstOrDefault()?.Columns;
+                rows = Arrays.Concat<T>(dataSets.Select((x) => x.Rows).ToArray());
+            }
+
+            return new T2() { KeyColumns = dataSets.FirstOrDefault()?.KeyColumns, Columns = cols, Rows = rows };
+        }
+    }
+}
