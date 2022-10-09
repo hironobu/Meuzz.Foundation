eb8d88ea11cba8431b67375feeeebd9cb457016a
hironobu <hironobu@users.noreply.github.com>
2022-10-09 20:34:40 +0900

    TypedTuple introduced

---
+++ Meuzz.Foundation/Types.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace Meuzz.Foundation
+{
+    public static class TypeExtensions
+    {
+        public static bool IsTuple(this Type tuple)
+        {
+            if (!tuple.IsGenericType)
+            {
+                return false;
+            }
+            var openType = tuple.GetGenericTypeDefinition();
+            return openType == typeof(ValueTuple<>)
+                || openType == typeof(ValueTuple<,>)
+                || openType == typeof(ValueTuple<,,>)
+                || openType == typeof(ValueTuple<,,,>)
+                || openType == typeof(ValueTuple<,,,,>)
+                || openType == typeof(ValueTuple<,,,,,>)
+                || openType == typeof(ValueTuple<,,,,,,>)
+                || (openType == typeof(ValueTuple<,,,,,,,>) && IsTuple(tuple.GetGenericArguments()[7]));
+        }
+    }
+
+    public static class TypedTuple
+    { 
+        public static object Make(object source)
+        {
+            var t = source.GetType();
+            if (!t.IsTuple())
+            {
+                return source;
+            }
+
+            var fields = t.GetFields(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+            var fieldValues = fields.Select(x => x.GetValue(source)).ToArray();
+            var fts = fieldValues.Select(v => v!.GetType()).ToArray();
+
+            Type tupleType;
+
+            switch (fts.Length)
+            {
+                case 1:
+                    tupleType = typeof(ValueTuple<>);
+                    break;
+                case 2:
+                    tupleType = typeof(ValueTuple<,>);
+                    break;
+                case 3:
+                    tupleType = typeof(ValueTuple<,,>);
+                    break;
+                case 4:
+                    tupleType = typeof(ValueTuple<,,,>);
+                    break;
+                case 5:
+                    tupleType = typeof(ValueTuple<,,,,>);
+                    break;
+                case 6:
+                    tupleType = typeof(ValueTuple<,,,,,>);
+                    break;
+                case 7:
+                    tupleType = typeof(ValueTuple<,,,,,,>);
+                    break;
+                case 8:
+                    tupleType = typeof(ValueTuple<,,,,,,,>);
+                    break;
+                default:
+                    throw new NotImplementedException();
+            }
+
+            tupleType = tupleType.MakeGenericType(fts);
+
+            // var tupleType = typeof(ValueTuple<,>).MakeGenericType(left!.GetType(), right!.GetType());
+            return Activator.CreateInstance(tupleType, fieldValues)!;
+        }
+    }
+}
